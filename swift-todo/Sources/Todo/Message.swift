ff956e8d7deef7930b3a1b9c80cc151b8b486ad6
giangduong36 <gduong@macalester.edu>
2018-04-13 14:08:52 -0500

    Update Swift part

--- swift-todo/Sources/Todo/Message.swift
+++ swift-todo/Sources/Todo/Message.swift
@@ -17,39 +17,37 @@ enum Message {
     func apply(to model: Model) -> Model {
         switch(self) {
             case .add:
-                var new_entries = model.entries
+                var model = model
                 if !model.newEntryField.isBlank() {
-                    new_entries.append(Entry(id: model.nextID, description: model.newEntryField))
+                    model.entries.append(Entry(id: model.nextID, description: model.newEntryField))
                 }
-                return Model(nextID: model.nextID + 1, newEntryField: "", entries: new_entries)
+                model.nextID += 1
+                model.newEntryField = ""
+                return model
 
             case .updateNewEntryField(let str):
-                return Model(nextID: model.nextID, newEntryField: str, entries: model.entries)
+                var model = model
+                model.newEntryField = str
+                return model
 
             case .check(let id, let isCompleted):
-                var new_entries = [Entry]()
-                for entry in model.entries {
-                    if(entry.id == id) {
-                        new_entries.append(Entry(id: entry.id,
-                                                 description: entry.description,
-                                                 completed: isCompleted))
-                    } else{
-                        new_entries.append(entry)
+                var model = model
+                for (index, _) in model.entries.enumerated() {
+                    if(model.entries[index].id == id) {
+                        model.entries[index].completed = isCompleted
                     }
                 }
-                return Model(nextID: model.nextID, newEntryField: model.newEntryField, entries: new_entries)
-            
+                return model
 
             case .delete(let id):
-                var new_entries = model.entries
-                new_entries.remove { $0.id == id }
-                return Model(nextID: model.nextID, newEntryField: model.newEntryField, entries: new_entries)
-
+                var model = model
+                model.entries.remove { $0.id == id }
+                return model
 
             case .deleteAllCompleted:
-                var new_entries = model.entries
-                new_entries.remove { $0.completed }
-                return Model(nextID: model.nextID, newEntryField: model.newEntryField, entries: new_entries)
+                var model = model
+                model.entries.remove { $0.completed }
+                return model
         }
     }
 }
