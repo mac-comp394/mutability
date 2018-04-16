c76a8f277e414197e8777f3ca9a2134b147b3be2
mattdavids <mdavids@macalester.edu>
2018-04-16 17:30:34 -0500

    Swift completed, your advice really helped

--- swift-todo/Sources/Todo/Message.swift
+++ swift-todo/Sources/Todo/Message.swift
@@ -14,30 +14,41 @@ enum Message {
     case delete(Int)
     case deleteAllCompleted
 
-    func apply(to model: Model) {
+    func apply(to model: Model) -> Model {
+        
+        var new_model: Model = model
+        
         switch(self) {
+            
             case .add:
-                if !model.newEntryField.isBlank() {
-                    model.entries.append(Entry(id: model.nextID, description: model.newEntryField))
+                if !new_model.newEntryField.isBlank() {
+                    new_model.entries.append(Entry(id: model.nextID, description: model.newEntryField))
                 }
-                model.nextID += 1
-                model.newEntryField = ""
+                new_model.nextID += 1
+                new_model.newEntryField = ""
 
             case .updateNewEntryField(let str):
-                model.newEntryField = str
+                new_model.newEntryField = str
 
             case .check(let id, let isCompleted):
-                for entry in model.entries {
+                var new_entries: [Entry] = []
+                
+                for entry in new_model.entries {
                     if(entry.id == id) {
-                        entry.completed = isCompleted
+                        new_entries.append(Entry(id: entry.id, description: entry.description, completed: isCompleted))
+                    } else {
+                        new_entries.append(entry)
                     }
                 }
+            
+                new_model.entries = new_entries
 
             case .delete(let id):
-                model.entries.remove { $0.id == id }
+                new_model.entries.remove { $0.id == id }
 
             case .deleteAllCompleted:
-                model.entries.remove { $0.completed }
+                new_model.entries.remove { $0.completed }
         }
+        return new_model
     }
 }
