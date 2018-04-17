f8bc1f0ac060765d8f99f419561df98f7b26d06b
kaylabeckm <kaylabeckm@gmail.com>
2018-04-17 00:02:06 -0500

    Swift model passing fixed. was not updating model in engine loop. Needed a new variable (which was messing up stuff in ruby)

--- swift-todo/Sources/Todo/Engine.swift
+++ swift-todo/Sources/Todo/Engine.swift
@@ -13,8 +13,10 @@ struct Engine {
     }
 
     static func runWithHistory(on model: Model, applying messages: [Message]) -> [Model] {
+        var new_model = model
         return messages.map { message in
-            return message.apply(to: model)
+            new_model = message.apply(to: new_model)
+            return new_model
         }
     }
 }
